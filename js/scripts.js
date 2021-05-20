86ea4cefaf2567b5fc693985624eff68374b37ae
amyjay1 <amynicolejackson@gmail.com>
2021-05-20 15:25:32 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
-console.log("Hi, welcome to my portfolio website. Live long and Prosper :-)")
-
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
+  if (x.className === 'navtoggle' && screen.width <640px){
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
